e37f0385e6247cc23409430502de417c9152f673
hyperclocker <hyperclocker@outlook.com>
2022-11-24 20:43:58 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias bat="batcat"
 alias clash="/home/hyperclocker/bin/clash"
 alias clash-update="/home/hyperclocker/bin/clash-update"
+alias prime-run="/home/hyperclocker/bin/prime-run"
 
 fish_vi_key_bindings
 
-set -U fish_greeting
+set -U fish_greeting 
